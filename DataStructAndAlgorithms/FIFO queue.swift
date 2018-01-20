0d0e8b0e864f6a395208c8d4ebafa04a0149aad4
🍍 <abc88788@hotmail.com>
2018-01-20 19:14:49 +0800

    feat(Data Structure & Algorithms): implement the collection protocol

--- DataStructAndAlgorithms/FIFO queue.swift
+++ DataStructAndAlgorithms/FIFO queue.swift
@@ -19,3 +19,30 @@ struct FIFOQueue<Element>:Queue{
         return operation.popLast()
     }
 }
+extension FIFOQueue:Collection{
+    var startIndex:Int {return 0}
+    var endIndex:Int {return operation.count + storage.count}
+    func index(after i: Int) -> Int {
+        precondition(i < endIndex)
+        return i + 1
+    }
+    subscript (position:Int)->Element{
+        precondition((startIndex..<endIndex).contains(position),"Must in the range")
+        if position < operation.endIndex{
+            return operation[operation.count - 1 - position]
+        }
+        return storage[position - operation.count]
+    }
+}
+extension FIFOQueue:ExpressibleByArrayLiteral{
+    init(arrayLiteral elements: Element...) {
+        self.init(storage: [], operation: elements.reversed())
+    }
+}
+extension FIFOQueue:CustomStringConvertible{
+    var description: String{
+        let element1 = self.operation.reversed().map{String(describing:$0)}.joined(separator: ",")
+        let element2 = self.storage.map{String(describing:$0)}.joined(separator: ",")
+        return "\(element1),\(element2)"
+    }
+}
