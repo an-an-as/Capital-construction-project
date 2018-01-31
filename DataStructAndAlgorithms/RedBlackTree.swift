e1c073e29f57052f38419d2556bbf8f3b07a7d43
🍍 <abc88788@hotmail.com>
2018-01-31 12:00:05 +0800

    perf(DataStructAndAlgorithms): implemented the collection types

--- DataStructAndAlgorithms/RedBlackTree.swift
+++ DataStructAndAlgorithms/RedBlackTree.swift
@@ -1,136 +1,226 @@
 import Foundation
 
-protocol SortedSet: BidirectionalCollection,CustomStringConvertible where Element:Comparable {
+protocol SortedSet: BidirectionalCollection, CustomStringConvertible where Element: Comparable {
     init()
-    func contain(_ element:Element) -> Bool
-    mutating func insert(_ newElement:Element)->(instered:Bool,memberAfterInsert:Element)
+    func contains(_ element: Element) -> Bool
+    mutating func insert(_ newElement: Element) -> (inserted: Bool, memberAfterInsert: Element)
 }
 extension SortedSet {
-    public var description: String{
+    public var description: String {
         let contents = self.lazy.map { "\($0)" }.joined(separator: ", ")
         return "[\(contents)]"
     }
 }
+
 public enum Color {
     case black
     case red
 }
-public enum RedBlackTree<Element:Comparable> {
+extension Color {
+    var symbol: String {
+        switch self {
+        case .black: return "◯"
+        case .red:   return "◉"
+        }
+    }
+}
+extension RedBlackTree: CustomStringConvertible {
+    func diagram(_ top: String, _ root: String, _ bottom: String) -> String {
+        switch self {
+        case .empty: return root + "●\n"
+        case let .node(color, value, .empty, .empty): return root + "\(color.symbol) \(value)\n"
+        case let .node(color, value, left, right):
+            return right.diagram(top + "    ", top + "┌───", top + "│   ")
+                + root + "\(color.symbol) \(value)\n"
+                + left.diagram(bottom + "│   ", bottom + "└───", bottom + "    ")
+        }
+    }
+    public var description: String {
+        return self.diagram("", "", "")
+    }
+}
+public enum RedBlackTree<Element: Comparable> {
     case empty
-    indirect case node(Color,Element,RedBlackTree,RedBlackTree)
+    indirect case node(Color, Element, RedBlackTree, RedBlackTree)
 }
 public extension RedBlackTree {
-    func contains(_ elements:Element)->Bool{
+    func contains(_ element: Element) -> Bool {
         switch self {
         case .empty: return false
-        case let .node(_,elements,_,_):return true
-        case let .node(_,value,left,_) where value > elements: return left.contains(elements)
-        case let .node(_,_,_,right): return right.contains(elements)
+        case .node(_, element, _, _): return true
+        case let .node(_, value, left, _) where value > element: return left.contains(element)
+        case let .node(_, _, _, right): return right.contains(element)
         }
     }
 }
 public extension RedBlackTree {
-    func forEach(_ body:(Element) throws -> Void) rethrows {
+    func forEach(_ body: (Element) throws -> Void) rethrows {
         switch self {
         case .empty: break
-        case let .node(_,value,left,right):
+        case let .node(_, value, left, right):
             try left.forEach(body)
             try body(value)
             try right.forEach(body)
         }
     }
 }
-extension Color {
-    var symbol: String{
-        switch self {
-        case .red: return "◉"
-        case .black: return "◯"
-        }
-    }
-}
-extension RedBlackTree:CustomStringConvertible {
-    func diagram(_ top:String, _ root:String,_ bottom:String)->String{
-        switch self {
-        case .empty: return root + "⚫︎\n"
-        case let .node(color,value,.empty,.empty): return root + "\(color.symbol)\(value)\n"
-        case let .node(color,value,left,right): return right.diagram(top + "    ",top + "┌───",top + "│   ")
-            + root + "\(color.symbol)\(value)\n"
-            + left.diagram(bottom + "│   ", bottom + "└───",bottom + "    " )
-        }
-    }
-    public var description: String{
-        return diagram("", "", "")
-    }
-}
 extension RedBlackTree {
     @discardableResult
-    public mutating func insert(_ element:Element)->(inserted:Bool,memberAfterInsert:Element){
-        let (tree,old) = changeToBlack(element)
+    public mutating func insert(_ element: Element) -> (inserted: Bool, memberAfterInsert: Element){
+        let (tree, old) = inserting(element)
         self = tree
         return (old == nil, old ?? element)
     }
 }
 extension RedBlackTree {
-    public func changeToBlack(_ element:Element)->(tree:RedBlackTree,existingMember:Element?){
-        let (tree,old) = inserting(element)
+    public func inserting(_ element: Element) -> (tree: RedBlackTree, existingMember: Element?) {
+        let (tree, old) = _inserting(element)
         switch tree {
-        case let .node(.red,value,left,right):
-            return (.node(.black,value,left,right),old)
+        case let .node(.red, value, left, right):
+            return (.node(.black, value, left, right), old)
         default:
-            return (tree,old)
+            return (tree, old)
         }
     }
 }
 extension RedBlackTree {
-    public func inserting (_ element:Element)->(tree:RedBlackTree,old:Element?){
+    func _inserting(_ element: Element) -> (tree: RedBlackTree, old: Element?){
         switch self {
         case .empty:
-            return (.node(.red,element,.empty,.empty),nil)
-        case let .node(_,oldValue,_,_) where oldValue == element:
-            return (self,oldValue)
-        case let .node(color,oldValue,left,right) where oldValue > element:
-            let(l,old) = left.inserting(element)
-            if let old = old  { return (self,old) }
-            return (blanced(color, oldValue, l, right),nil)
-        case let .node(color,oldValue,left,right):
-            let (r,old) = right.inserting(element)
-            if let old = old { return (self,old) }
-            return (blanced(color, oldValue, left, r),nil)
+            return (.node(.red, element, .empty, .empty), nil)
+        case let .node(_, value, _, _) where value == element:
+            return (self, value)
+        case let .node(color, value, left, right) where value > element:
+            let (l, old) = left._inserting(element)
+            if let old = old { return (self, old) }
+            return (balanced(color, value, l, right), nil)
+        case let .node(color, value, left, right):
+            let (r, old) = right._inserting(element)
+            if let old = old { return (self, old) }
+            return (balanced(color, value, left, r), nil)
         }
     }
 }
 extension RedBlackTree {
-    func blanced(_ color:Color,_ value:Element,_ left:RedBlackTree,_ right:RedBlackTree) -> RedBlackTree {
-        switch (color,value,left,right) {
-        case let (.black,z,.node(.red,y,.node(.red,x,a,b),c),d):
-            return .node(.black,y,.node(.red,x,a,b),.node(.red,z,c,d))
-        case let (.black,z,.node(.red,x,.node(.red,y,b,c),a),d):
-            return .node(.black,z,.node(.red,x,a,b),.node(.red,y,c,d))
-        case let (.black,x,a,.node(.red,y,b,.node(.red,z,c,d))):
-            return .node(.black,z,.node(.red,x,a,b),.node(.red,y,c,d))
-        case let (.black,x,a,.node(.red,z,d,.node(.red,y,b,c))):
-            return .node(.black,z,.node(.red,x,a,b),.node(.red,y,c,d))
+    func balanced(_ color: Color, _ value: Element, _ left: RedBlackTree, _ right: RedBlackTree) -> RedBlackTree {
+        switch (color, value, left, right) {
+        case let (.black, z,.node(.red, y, .node(.red, x, a, b),c),d):
+            return .node(.red, y, .node(.black, x, a, b), .node(.black, z, c, d))
+        case let (.black, z, .node(.red, x, a, .node(.red, y, b,c)),d):
+            return .node(.red, y, .node(.black, x, a, b), .node(.black, z, c, d))
+        case let (.black, x, a, .node(.red, z, .node(.red, y, b, c), d)):
+            return .node(.red, y, .node(.black, x, a, b), .node(.black, z, c, d))
+        case let (.black, x, a, .node(.red, y, b, .node(.red, z, c, d))):
+            return .node(.red, y, .node(.black, x, a, b), .node(.black, z, c, d))
         default:
-            return .node(color,value,left,right)
+            return .node(color, value, left, right)
         }
     }
 }
 extension RedBlackTree {
-    public init(){
+    public struct Index {
+        fileprivate var value: Element?
+    }
+}
+extension RedBlackTree.Index: Comparable {
+    public static func ==(left: RedBlackTree<Element>.Index, right: RedBlackTree<Element>.Index) -> Bool {
+        return left.value == right.value
+    }
+    public static func <(left: RedBlackTree<Element>.Index, right: RedBlackTree<Element>.Index) -> Bool {
+        if let lv = left.value, let rv = right.value {
+            return lv < rv
+        }
+        return left.value != nil
+    }
+}
+extension RedBlackTree {
+    func min() -> Element? {
+        switch self {
+        case .empty:
+            return nil
+        case let .node(_, value, left, _):
+            return left.min() ?? value
+        }
+    }
+}
+extension RedBlackTree {
+    func max() -> Element? {
+        var node = self
+        var maximum: Element? = nil
+        while case let .node(_, value, _, right) = node {
+            maximum = value
+            node = right
+        }
+        return maximum
+    }
+}
+extension RedBlackTree: Collection {
+    public var startIndex: Index { return Index(value: self.min()) }
+    public var endIndex: Index { return Index(value: nil) }
+    public subscript(i: Index) -> Element {
+        return i.value!
+    }
+}
+extension RedBlackTree: BidirectionalCollection {
+    public func formIndex(after i: inout Index) {
+        let v = self.value(following: i.value!)
+        precondition(v.found,"Out of range")
+        i.value = v.next
+    }
+    public func index(after i: Index) -> Index {
+        let v = self.value(following: i.value!)
+        precondition(v.found)
+        return Index(value: v.next)
+    }
+}
+extension RedBlackTree {
+    func value(following element: Element) -> (found: Bool, next: Element?) {
+        switch self {
+        case .empty:
+            return (false, nil)
+        case .node(_, element, _, let right):
+            return (true, right.min())
+        case let .node(_, value, left, _) where value > element:
+            let result = left.value(following: element)
+            return (result.found, result.next ?? value)
+        case let .node(_, _, _, right):
+            return right.value(following: element)
+        }
+    }
+}
+extension RedBlackTree {
+    public func formIndex(before i: inout Index) {
+        let v = self.value(following: i.value!)
+        precondition(v.found)
+        i.value = v.next
+    }
+    public func index(before i: Index) -> Index {
+        let v = self.value(following: i.value!)
+        precondition(v.found)
+        return Index(value: v.next)
+    }
+}
+extension RedBlackTree {
+    public var count: Int {
+        switch self {
+        case .empty:
+            return 0
+        case let .node(_, _, left, right):
+            return left.count + 1 + right.count
+        }
+    }
+}
+extension RedBlackTree: SortedSet {
+    public init() {
         self = .empty
     }
 }
 
-let emptyTree: RedBlackTree<Int> = .empty
+var tree:RedBlackTree<Int> = RedBlackTree()
+tree.insert(0)
+tree.insert(1)
+tree.insert(2)
+tree.insert(3)
 
-let bigTree: RedBlackTree<Int> =
-    .node(.black, 9,
-          .node(.red, 5,
-                .node(.black, 1, .empty, .node(.red, 4, .empty, .empty)),
-                .node(.black, 8, .empty, .empty)),
-          .node(.red, 12,
-                .node(.black, 11, .empty, .empty),
-                .node(.black, 16,
-                      .node(.red, 14, .empty, .empty),
-                      .node(.red, 17, .empty, .empty))))
-print(bigTree)
+tree.reduce(0,+)
+print(tree)
