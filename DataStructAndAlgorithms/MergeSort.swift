daef06e54c16992a4677a93ff8b996fd7525cae3
🍍 <abc88788@hotmail.com>
2018-02-08 12:08:20 +0800

    refactor(DataStructAndAlgorithms): a new version for merge sort

--- DataStructAndAlgorithms/MergeSort.swift
+++ DataStructAndAlgorithms/MergeSort.swift
@@ -1,48 +1,109 @@
 import Foundation
-func mergeSort( _ arr:inout [Int]) -> [Int] {
-    func merge (_ arr: inout[Int],  low: Int, mid: Int, high: Int, temp:inout [Int]) {
-        var i = low
-        var j = mid + 1
-        let m = mid
-        let n = high
-        var k = 0
-        while (i <= m && j <= n) {
-            if (arr[i] <=  arr[j]) {
-                temp[k] = arr[i]
-                k += 1
-                i += 1
+extension Array where Element: Comparable {
+    private mutating func merge(left: Int, middle: Int, right: Int) {
+        var tempArray: [Element] = []
+        var l = left, m = middle
+        while l != middle && m != right {
+            if self[m] < self[l] {
+                tempArray.append(self[m])
+                m += 1
             }
-            else
-            {
-                temp[k] = arr[j]
-                k += 1
-                j += 1
+            else {
+                tempArray.append(self[l])
+                l += 1
             }
         }
-        while i <= m {
-            temp[k] = arr[i]
-            k += 1
-            i += 1
-        }
-        while j <=  n {
-            temp[k] = arr[j]
-            k += 1
-            j += 1
-        }
-        for f in 0 ..< k {
-            arr[low + f] = temp[f]
+        tempArray.append(contentsOf: self[l..<middle])
+        tempArray.append(contentsOf: self[m..<right])
+        replaceSubrange(left..<right, with: tempArray)
+    }
+    mutating func mergeSortInPlaceInefficient() {
+        let arrayCount = count
+        var size = 1
+        while size < arrayCount {
+            for strideLeft in stride(from: 0, to: arrayCount - size, by: size*2) {
+                merge(left: strideLeft, middle: (strideLeft+size), right: Swift.min(strideLeft+size*2,arrayCount))
+            }
+            size *= 2
         }
     }
-    func internalMergeSort(_ arr: inout[Int],  low: Int,  high: Int,   temp: inout[Int]) {
-        if high <= low {
-            return
-        }
-        let mid = low + (high - low) / 2
-        internalMergeSort(&arr, low: low, high: mid, temp: &temp)
-        internalMergeSort(&arr, low: mid + 1, high: high, temp: &temp)
-        merge(&arr, low: low, mid: mid, high: high, temp: &temp)
-    }
-    var temp: [Int] = arr
-    internalMergeSort(&arr, low: 0, high: arr.count - 1, temp: &temp)
-    return arr
 }
+var array = [3,1,2,5,6]
+array.mergeSortInPlaceInefficient()
+
+Swift.min(10, 100)
+/**
+ [0][1][2][3][4]
+ 3  1  2  5  6
+ 
+ *size1
+ lo:0 mi:1 hi:2   [0][1][2]
+ l = lo:0          3  1  2
+ m = mi:1
+ l != m && m != hi
+ self[m]<self[l] r+=1
+ l:0 m:2 h:2     temp:[1]
+ self[l..<mi]= 3 temp:[1,3]
+ self[m..<hi]= 2 temp:[1,3]
+ replaceSubrange:lo..<hi
+ [0][1] [2][3][4]
+ 1  3   2  5  6
+ 
+ lo:2 mi:3 hi:4   [2][3][4]
+ l = lo:2          2  5  6
+ m = mi:3
+ l != m && m != hi
+ self[m]<self[l] m+=1
+ else l+=1       temp[2]
+ l:3 m:3 r:4
+ self[l..<mi]= 3 temp:[2,5]
+ self[m..<hi]= 2 temp:[2,5]
+ replaceSubrange: lo..<hi
+ [0][1] [2][3] [4]
+ 1  3   2  5   6
+ 
+ *size2
+ lo:0 mi:2 hi:4   [0][1] [2] [3][4]
+ l = lo:0          1  3   2   5  6
+ m = mi:2
+ l != m && m != hi
+ self[m]<self[l] m+=1       self[2]<self[1]  temp[1,2]   m+=1  m3 != 4
+ else l+=1  1!=2  temp[1]
+ l+1 = mid2
+ l:2 m:3 r:4
+ self[l..<mi]= 3 temp:[]
+ self[m..<hi]= 2 temp:[1,2,3,5]
+ replaceSubrange: lo..<hi
+ [0][1] [2][3] [4]
+ 1  2   3  5   6
+ 
+ lo:2 mi:4 hi:5   [0][1][2][3] [4]
+ l = lo:0          1  2  3  5   6
+ m = mi:4
+ l != m && m != hi
+ self[m]<self[l] m+=1
+ else l+=1  1!=2  temp[1,2,3,5]
+ 
+ l:4 m:4 r:4
+ self[l..<mi]= 3 temp:[]
+ self[m..<hi]= 2 temp:[1,2,3,5,6]
+ replaceSubrange: lo..<hi
+ [0][1] [2][3] [4]
+ 1  2   3  5   6
+ 
+ *size4
+ lo:0 mi:4 hi:5   [0][1] [2] [3][4]
+ l = lo:0          1  2   3   5  6
+ m = mi:2
+ l != m && m != hi
+ self[m]<self[l] m+=1
+ else l+=1  1!=2  temp[1]
+ l+1 = mid2
+ l:2 m:3 r:4
+ self[l..<mi]= 3 temp:[]
+ self[m..<hi]= 2 temp:[1,2,3,5]
+ replaceSubrange: lo..<hi
+ [0][1] [2][3] [4]
+ 1  2   3  5   6
+ 
+ */
