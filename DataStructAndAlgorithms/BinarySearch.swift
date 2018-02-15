ef5675912b5afcab8e070f816d119934ade3caed
🍍 <abc88788@hotmail.com>
2018-02-15 15:39:44 +0800

    style(DataStructAndAlgorithms/BinarySearch): update for SwiftLint

--- DataStructAndAlgorithms/BinarySearch.swift
+++ DataStructAndAlgorithms/BinarySearch.swift
@@ -1,33 +1,25 @@
-import Foundation
-
 extension RandomAccessCollection {
-    public func binarySearch(for value: Element,  precondition increasingOrder:(Element,Element)->Bool)->Index?{
-        guard !isEmpty else {return nil}
+    public func binarySearch(value: Element, precondition: (Element,Element) -> Bool) -> Index? {
+        guard !isEmpty else { return nil }
         var left = startIndex
         var right = index(before: endIndex)
         while left <= right {
-            let dist  = distance(from: left, to: right)
-            let mid = index(left, offsetBy: dist/2)
-            let candidate = self[mid]
-            if increasingOrder(candidate,value){
+            let steps = distance(from: left, to: right)
+            let mid = index(left, offsetBy: steps / 2)
+            let candicate = self[mid]
+            if precondition(candicate, value) {
                 left = index(after: mid)
-            }
-            else if increasingOrder(value,candidate){
+            } else if precondition(value, candicate) {
                 right = index(before: mid)
-            }
-            else{
+            } else {
                 return mid
             }
         }
         return nil
     }
 }
-extension RandomAccessCollection where Element:Comparable{
-    func binarySearch(for value:Element)->Index?{
-        return binarySearch(for: value, precondition: <)
+extension RandomAccessCollection where Element: Comparable {
+    func binarySearch(value: Element) -> Index? {
+        return binarySearch(value: value, precondition: <)
     }
 }
-
-var array = [1,2,3,4,5]
-array.binarySearch(for: 5) == 4
-array.reversed().binarySearch(for: 5, precondition: >) == 0
