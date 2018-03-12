beb536bd9634541d6c6d9a46f5d65fe6b25323c4
🍍 <abc88788@hotmail.com>
2018-03-12 09:10:37 +0800

    feat(StringExtensions): add split method

--- Topics/ProgrammingLanguagesWithDifferences/Unicode/StringExtensions.swift
+++ Topics/ProgrammingLanguagesWithDifferences/Unicode/StringExtensions.swift
@@ -4,3 +4,25 @@ extension String {
         }
     }
 }
+extension String {
+    func wrapped(after: Int = 70) -> String {
+        var i = 0
+        let lines = self.split(omittingEmptySubsequences: false) {
+            character in
+            switch character {
+            case "\n", " " where i >= after:
+                i = 0
+                return true
+            default:
+                i += 1
+                return false
+            }
+        }
+        return lines.joined(separator: "\n")
+    }
+}
+extension Collection where Element: Equatable {
+    func split<S: Sequence>(separators: S) -> [SubSequence] where Element == S.Element {
+        return split { separators.contains($0) }
+    }
+}
