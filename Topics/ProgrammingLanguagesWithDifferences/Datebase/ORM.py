55f21b863e56e2e4a58e524758f14e0d604e1d35
🍍 <abc88788@hotmail.com>
2018-03-17 23:53:34 +0800

    feat(Database): add select method

--- Topics/ProgrammingLanguagesWithDifferences/Datebase/ORM.py
+++ Topics/ProgrammingLanguagesWithDifferences/Datebase/ORM.py
@@ -20,3 +20,16 @@ async def create_pool(loop, **kw):
                                         minsize=kw.get('minsize', 1),
                                         loop=loop
                                         )
+
+async def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    async with __pool.acquire() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+    logging.info('rows returned: %s' % len(rs))
+        return rs
