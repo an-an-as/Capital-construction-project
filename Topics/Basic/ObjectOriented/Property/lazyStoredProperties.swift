66faee92dc1eedb46de697be55f02ee2a82cdab8
🍍 <abc88788@hotmail.com>
2018-05-04 21:17:30 +0800

    feat(Property): add lazyStoredProperties

---
+++ Topics/Basic/ObjectOriented/Property/lazyStoredProperties.swift
@@ -0,0 +1,14 @@
+class DataImporter {
+    var fileName = "data.txt"
+}
+
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+}
+
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+
+print(manager.importer.fileName)
