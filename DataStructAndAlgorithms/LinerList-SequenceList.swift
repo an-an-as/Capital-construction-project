d75563a37b86d2553837420068d07d09155185ab
🍍 <abc88788@hotmail.com>
2018-05-20 21:24:46 +0800

    perf(LinerList-SequenceList.swift): update the content

--- DataStructAndAlgorithms/LinerList-SequenceList.swift
+++ DataStructAndAlgorithms/LinerList-SequenceList.swift
@@ -5,73 +5,72 @@
  3.线性表是数据结构中的逻辑结构。可以存储在数组上(顺序存储)，也可以存储在链表上(链式存储),用数组来存储的线性表就是顺序表
  4.数组通过系统给数组分配了一块内存。线性表的大小是可变动态分配
  - 末尾追加元素
- - 根据下标索引元素
+ - 索引元素下标
  - 根据下标修改元素
- - 根据下标插入值
- - 根据下标移除元素
+ - 根据下标插入元素
+ - 移除元素
  */
 import Foundation
-public class SequenceList<Element> {
+public struct SequenceList<Element:Equatable> {
     private var list: NSMutableArray
     private(set) public var count = 0
     public var capacity = 0
-    public init(capacity: Int) {
+    init(capacity:Int){
         self.capacity = capacity
         list = NSMutableArray(capacity: capacity)
     }
-}
-extension SequenceList {
-    func append(_ newElement: Element) {
-        list[count] = newElement
+    func fetchValue(_ index:Int) -> Element? {
+        assert(index >= 0 && index < count)
+        
+        return list[index] as? Element
+    }
+    mutating func append(newValue value:Element) {
+        list[count] = value
         count += 1
     }
-    @discardableResult
-    func fetchValue(_ index: Int) -> Element? {
-        assert(index >= 0 && index < count, "Invalid index")
-        return list[index] as? Element /// NSMutableArray: Any
-    }
-    @discardableResult
-    func updateValue(_ index: Int, value: Element) -> Element? {
-        assert(index >= 0 && index < count, "Invalid index")
-        let oldValue = list[index]
-        list[index] = value
-        return oldValue as? Element
-    }
-    func insert(_ newElement: Element, index: Int)  {
-        assert(index >= 0 && index < count, "Invalid index")
+    mutating func insert(newElement element:Element, at index:Int) {
+        assert(index >= 0 && index < count)
         var endIndex = count
-        ///元素后移操作0 1 2 3 endIndex index = 1 < 4
         while index < endIndex {
-            list[endIndex] = list[endIndex-1]
+            list[endIndex] = list[endIndex - 1]
             endIndex -= 1
         }
-        list[index] = newElement
+        list[index] = element
         count += 1
     }
-    func remove(_ index: Int)  {
-        assert(index >= 0 && index < count, "Invalid index")
-        for i in index..<count-1 {
-            list[i] = list[i+1]
+    mutating func remove(element:Element) {
+        index(element: element).map {
+            for cursor in $0..<count - 1 {
+                list[cursor] = list[cursor + 1]
+            }
         }
-        count -= 1
         list.removeLastObject()
+        count -= 1
+    }
+    private func index(element:Element) -> Int? {
+        var currentIndex = 0
+        while currentIndex < count - 1 {
+            if list[currentIndex] as! Element == element  {
+                return currentIndex
+            } else {
+                currentIndex += 1
+            }
+        }
+        return nil
     }
 }
-extension SequenceList:CustomDebugStringConvertible {
-    public var debugDescription: String {
+extension SequenceList: CustomDebugStringConvertible {
+    public var debugDescription: String{
         var str = ""
-        for (index,value) in list.enumerated(){
-            str += "index:\(index) -> \(value)" + "\n"
+        for (i,element) in list.enumerated() {
+            str += "index:\(i) -> \(element)" + "\n"
         }
         return str
     }
 }
-///test
 var arr = SequenceList<Int>(capacity: 5)
-arr.append(3)
-arr.append(1)
-arr.remove(0)
-arr.updateValue(0, value: 10)
-arr.insert(100, index: 0)
-arr.fetchValue(0)
-print(arr.debugDescription,arr.count)
+arr.append(newValue: 1)
+arr.append(newValue: 3)
+arr.insert(newElement: 2, at: 1)
+arr.remove(element: 1)
+print(arr.debugDescription,arr.count,arr.capacity)
