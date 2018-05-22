2a5e26c6aee15ec8ec5b5fc135bb46032444b281
🍍 <abc88788@hotmail.com>
2018-05-22 10:39:31 +0800

    feat(MergeSort.swift): update the content

--- DataStructAndAlgorithms/MergeSort.swift
+++ DataStructAndAlgorithms/MergeSort.swift
@@ -1,112 +1,119 @@
+/********************************************************************************************************************************************
+ 归并排序主要用了分治法的思想,将需要排序的数组进行拆分，将其拆分的足够小。当拆分的数组中只有一个元素时，则这个拆分的数组是有序的。
+ 然后将这些有序的数组进行两两合并，在合并过程中进行比较，合并生成的新的数组仍然是有序的。
+ 然后再次将合并的有序数组进行合并，重复这个过程，直到整个数组是有序的。
+ 
+ 
+ ********************************************************************************************************************************************
+ [1,3,2,4,5,9,7,10,6,8]
+ 0 1 2 3 4 5 6 7  8 9
+ siza < count      排序好的元素数不能越界
+ size *= 2         排序好的元素数量
+ size * 2          排序好的元素 * 2  进行合并确定比较范围 *2表示需要比较两个已经排序好的
+ count - size      确定在count内所能达到的最大比较范围次数  如果排序好的size为2 需要比较的每组范围是4  能生成比较组的数  4   8   12  16
+ stride            每次遍历出的基准范围
+ stride + size     确定下一个比较单元
+ stride + size * 2 下一组需要比较的 当前比较单元和下一组的关系 如果size4 范围8 count5 越界 此时取count5 结束比较
+ 
+ stride != stride + size &&  stride + size !=stride + size * 2 确定两组比较单元独立  eg: 36 27
+ self[stride] > self[stride + size]  左右均为有序 如果左第一个数 > 右第一个数 自然右剩下的就比左第一个数小  stride + size  += 1 加入tempArray
+ else stride += 1
+ tempArray加入剩余的 stride..< stride + size  ; stride + seize ..< stride + size * 2
+ 替换原数组stride..<stride + size * 2
+ 
+ 
+ ********************************************************************************************************************************************
+ 1.拆分为单个两两比较后
+ Split:         [1,3,   2,4    5,9   7,10   6,8]
+ ArrayIndex:     01     23     45     67     89
+ Compare:
+ Merge:
+ Result:        [1,3,   2,4    5,9   7,10   6,8]
+ 
+ 
+ 2. 再将已经成对的 两两比较
+ Split:       [1,2,3,4,   5,7,9,10    6,8]
+ ArrayIndex:    0123        4567      8 9
+ 
+ 
+ 3. 重复
+ [1,2,3,4,5,7,9,10   6,8]          size4
+ 
+ 
+ 4. 最后
+ [1,2,3,4,5,6,7,8,9,10]            size8
+ 
+ size 1
+ stride = size * 2       0   2   4   6   8  to count(10)-size = 9
+ left:stride             mid:stride + size                      end: stride + size * 2  |  count(10)
+ 0                       1                                      2    if   left != mid   mid != end   self[left] > self[mid]   tempArray.append(self[mid])   mid + 1
+ 2                       3                                      4    else tempArray.append(self[left])  left + 1
+ 4                       5                                      7    temp [1]
+ 6                       7                                      9    tempArray.append(contentsOf: self[l..<middle])    left  += 1  返回空
+ 8                       9                                      10   tempArray.append(contentsOf: self[m..<endIndex])  right += 1  返回空   temp[1,3]
+ replaceSubrange(left..<endIndex, with: tempArray) 替换原有数组
+ [1,  3,   2,   4    5,    9   7,  10   6,  8]
+ >0   1   -2-   3<   4    5    6   7<   8   9
+ size 2
+ stride = size * 2       0   4              to count(10)-size = 8
+ left:stride             mid:stride + size                      end: stride + size * 2  |  count(10)
+ 0                       2                                      4   if  left != mid   mid != end   self[left] > self[mid]   tempArray.append(self[mid])   mid + 1 temp:[1,2]
+ 4                       6                                      8   else tempArray.append(self[left])  left + 1  temp: [1]  temp: [1,2,3]
+ _                                                                  tempArray.append(contentsOf: self[l..<middle])    left  += 1  返回空
+ 8                       10                                     12  tempArray.append(contentsOf: self[m..<endIndex])    mid != end  temp[1,3] temp: [1,2,3,4]
+ temp [1,2,3,4,  5,7,9,10]
+ replaceSubrange(left..<endIndex, with: tempArray) 替换原有数组
+ [1,  2,  3,  4,   5,  7,  9,  10    6,  8]
+ >0   1   2   3   -4-  5   6    7    8<  9
+ size 4
+ stride = size * 2       0   8              to count(10)-size = 6
+ left:stride             mid:stride + size                      end: stride + size * 2  |  count(10)
+ 0                       4                                      8    temp[1,2,3,4,5]
+ [1,  2,  3,  4,   5,  7,  9,  10    6,   8]
+ >0   1   2   3    4   5   6    7   -8-   9
+ 
+ 
+ size 8
+ stride = size * 2       0   8              to count(10)-size = 2
+ left:stride             mid:stride + size                      end: stride + size * 2  |  count(10)
+ 0                       8                                      count = 10
+ [1,2,3,4,5,6]
+ [1,2,3,4,5,6,7,8]
+ [1,2,3,4,5,6,7,8,9,10]
+ 
+ ************************************************************************************************************************************************************************/
 import Foundation
 extension Array where Element: Comparable {
     mutating func mergeSortInPlace() {
         var tempArray:[Element] = []
         tempArray.reserveCapacity(count)
-        
-        func merge(left: Int, middle: Int, endIndex: Int) {
-            tempArray.removeAll(keepingCapacity: true)
-            var l = left, m = middle
-            while l != middle && m != endIndex {
-                if  self[l] > self[m]  {
-                    tempArray.append(self[m])
-                    m += 1
+        func merge(currentStrideIndex: Int, nextCompareUnitIndex: Int, endIndex: Int) {
+            tempArray.removeAll(keepingCapacity: true)  /// 在函数内捕获局部变量
+            var cursorL = currentStrideIndex, cursorR = nextCompareUnitIndex
+            while cursorL != nextCompareUnitIndex && cursorR != endIndex {
+                if  self[cursorL] > self[cursorR]  {
+                    tempArray.append(self[cursorR])
+                    cursorR += 1
                 }
                 else {
-                    tempArray.append(self[l])
-                    l += 1
+                    tempArray.append(self[cursorL])
+                    cursorL += 1
                 }
             }
-            tempArray.append(contentsOf: self[l..<middle])
-            tempArray.append(contentsOf: self[m..<endIndex])
-            print(tempArray)
-            replaceSubrange(left..<endIndex, with: tempArray)
+            tempArray.append(contentsOf: self[cursorL..<nextCompareUnitIndex])
+            tempArray.append(contentsOf: self[cursorR..<endIndex])
+            replaceSubrange(currentStrideIndex..<endIndex, with: tempArray)
         }
-        
         let arrayCount = count
         var size = 1
         while size < arrayCount {
-            for strideLeft in stride(from: 0, to: arrayCount - size, by: size*2) {
-                merge(left: strideLeft, middle: (strideLeft+size), endIndex: Swift.min(strideLeft+size*2,arrayCount))
+            for currentStride in stride(from: 0, to: arrayCount - size, by: size * 2) {
+                merge(currentStrideIndex: currentStride, nextCompareUnitIndex: (currentStride + size), endIndex: Swift.min(currentStride + size * 2, arrayCount))
             }
             size *= 2
         }
     }
 }
-var array = [1,3,2,4,5]
+var array =  [1,3,2,4,5,9,7,10,6,8]
 array.mergeSortInPlace()
-/**
- [0][1][2][3][4]
- 3  1  2  5  6
- 
- *size1
- lo:0 mi:1 hi:2   [0][1][2]
- l = lo:0          3  1  2
- m = mi:1
- l != m && m != hi
- self[m]<self[l] r+=1
- l:0 m:2 h:2     temp:[1]
- self[l..<mi]= 3 temp:[1,3]
- self[m..<hi]= 2 temp:[1,3]
- replaceSubrange:lo..<hi
- [0][1] [2][3][4]
- 1  3   2  5  6
- 
- lo:2 mi:3 hi:4   [2][3][4]
- l = lo:2          2  5  6
- m = mi:3
- l != m && m != hi
- self[m]<self[l] m+=1
- else l+=1       temp[2]
- l:3 m:3 r:4
- self[l..<mi]= 3 temp:[2,5]
- self[m..<hi]= 2 temp:[2,5]
- replaceSubrange: lo..<hi
- [0][1] [2][3] [4]
- 1  3   2  5   6
- 
- *size2
- lo:0 mi:2 hi:4   [0][1] [2] [3][4]
- l = lo:0          1  3   2   5  6
- m = mi:2
- l != m && m != hi
- self[m]<self[l] m+=1       self[2]<self[1]  temp[1,2]   m+=1  m3 != 4
- else l+=1  1!=2  temp[1]
- l+1 = mid2
- l:2 m:3 r:4
- self[l..<mi]= 3 temp:[]
- self[m..<hi]= 2 temp:[1,2,3,5]
- replaceSubrange: lo..<hi
- [0][1] [2][3] [4]
- 1  2   3  5   6
- 
- lo:2 mi:4 hi:5   [0][1][2][3] [4]
- l = lo:0          1  2  3  5   6
- m = mi:4
- l != m && m != hi
- self[m]<self[l] m+=1
- else l+=1  1!=2  temp[1,2,3,5]
- 
- l:4 m:4 r:4
- self[l..<mi]= 3 temp:[]
- self[m..<hi]= 2 temp:[1,2,3,5,6]
- replaceSubrange: lo..<hi
- [0][1] [2][3] [4]
- 1  2   3  5   6
- 
- *size4
- lo:0 mi:4 hi:5   [0][1] [2] [3][4]
- l = lo:0          1  2   3   5  6
- m = mi:2
- l != m && m != hi
- self[m]<self[l] m+=1
- else l+=1  1!=2  temp[1]
- l+1 = mid2
- l:2 m:3 r:4
- self[l..<mi]= 3 temp:[]
- self[m..<hi]= 2 temp:[1,2,3,5]
- replaceSubrange: lo..<hi
- [0][1] [2][3] [4]
- 1  2   3  5   6
- 
- */
+print(array)
