0b55b65b5c912ecf9a9dff1076e064cff01670fd
🍍 <abc88788@hotmail.com>
2018-05-31 18:29:39 +0800

    chore(ShuffleSequence.swift): update the content

--- DataStructAndAlgorithms/ShuffleSequence.swift
+++ DataStructAndAlgorithms/ShuffleSequence.swift
@@ -1,12 +1,7 @@
-import Foundation
-//:  ["A","B","C"]  遍历每个值  从每个值到 endIndex的距离中 得出随机数
-//:  根据随机数在每个值的下标基础上偏移   交换当前值的下标 和 偏移值的下标
-//:  遍历不能到 endIndex 前面的一个数 如果endIndex前一个数 ~~ endIndex distance为1 arcRandom1 为0  就不会产生偏移值
 //:  IndexDistance 不确定具体类型 不一定是Int32 需要创建一个对所有实现了 BinaryInteger 协议的整型类型都适用的 arc4random_uniform
 //:  arc4random_uniform only 接收UInt32 类型 callable up to UInt32.max
 //:  BinaryInteger 面向协议的整数实现 IndexDistance 遵循该协议
 //:  RangeReplaceableCollection 可以创建一个新的空集合，以及可以将任意序列 (在这里，就是 self) 添加到空集合的后面。这保证了可以进行完全的复制。
-
 import Foundation
 extension BinaryInteger{
     static func arc4random_uniform(_ upper_bound:Self)->Self{
@@ -17,19 +12,19 @@ extension BinaryInteger{
 }
 extension MutableCollection where Self:RandomAccessCollection{
     mutating func shuffle(){
-        var i = startIndex
+        var cursorL = startIndex
         let beforeEndIndex = index(before: endIndex)
-        while i < beforeEndIndex {
-            let dist = distance(from: i, to: endIndex)
+        while cursorL < beforeEndIndex {
+            let dist = distance(from: cursorL, to: endIndex)
             let randomDistance = Int.arc4random_uniform(dist)
             ///IndexDistance' is deprecated: all index distances are now of type Int
-            let j = index(i, offsetBy: randomDistance)
-            self.swapAt(i, j)
-            formIndex(after: &i)
+            let randomOffset = index(cursorL, offsetBy: randomDistance)
+            self.swapAt(cursorL, randomOffset)
+            formIndex(after: &cursorL)
         }
     }
 }
-extension MutableCollection where Self: RandomAccessCollection, Self: RangeReplaceableCollection {
+extension MutableCollection where Self: RandomAccessCollection, Self: RangeReplaceableCollection  {
     func shuffled() -> Self {
         var clone = Self()
         clone.append(contentsOf: self)
