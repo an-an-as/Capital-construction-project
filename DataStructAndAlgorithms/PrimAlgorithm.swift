b9040cdeea2990aae1171c537cb7cfca471cf084
🍍 <abc88788@hotmail.com>
2018-10-05 10:05:51 +0800

    perf(PrimAlgorithm.swift): update the content

--- DataStructAndAlgorithms/PrimAlgorithm.swift
+++ DataStructAndAlgorithms/PrimAlgorithm.swift
@@ -1,27 +1,20 @@
-// Undirected edge
 public struct Edge<T>: CustomStringConvertible {
     public let vertex1: T
     public let vertex2: T
     public let weight: Int
-    
     public var description: String {
         return "[\(vertex1)-\(vertex2), \(weight)]"
     }
 }
-
-// Undirected weighted graph
 public struct Graph<T: Hashable>: CustomStringConvertible {
-    
     public private(set) var edgeList: [Edge<T>]
     public private(set) var vertices: Set<T>
     public private(set) var adjList: [T: [(vertex: T, weight: Int)]]
-    
     public init() {
         edgeList = [Edge<T>]()
         vertices = Set<T>()
         adjList = [T: [(vertex: T, weight: Int)]]()
     }
-    
     public var description: String {
         var description = ""
         for edge in edgeList {
@@ -29,33 +22,24 @@ public struct Graph<T: Hashable>: CustomStringConvertible {
         }
         return description
     }
-    
     public mutating func addEdge(vertex1 v1: T, vertex2 v2: T, weight w: Int) {
         edgeList.append(Edge(vertex1: v1, vertex2: v2, weight: w))
         vertices.insert(v1)
         vertices.insert(v2)
-        
         adjList[v1] = adjList[v1] ?? []
         adjList[v1]?.append((vertex: v2, weight: w))
-        
         adjList[v2] = adjList[v2] ?? []
         adjList[v2]?.append((vertex: v1, weight: w))
     }
-    
     public mutating func addEdge(_ edge: Edge<T>) {
         addEdge(vertex1: edge.vertex1, vertex2: edge.vertex2, weight: edge.weight)
     }
 }
 
 /*
- Priority Queue, a queue where the most "important" items are at the front of
- the queue.
- 
- The heap is a natural data structure for a priority queue, so this object
- simply wraps the Heap struct.
- 
+ Priority Queue, a queue where the most "important" items are at the front of the queue.
+ The heap is a natural data structure for a priority queue, so this object simply wraps the Heap struct.
  All operations are O(lg n).
- 
  Just like a heap can be a max-heap or min-heap, the queue can be a max-priority
  queue (largest element first) or a min-priority queue (smallest element first).
  */
@@ -145,9 +129,6 @@ func minimumSpanningTreePrim<T>(graph: Graph<T>) -> (cost: Int, tree: Graph<T>) 
     
     return (cost: cost, tree: tree)
 }
-
-
-
 
 print("===== Prim's =====")
 let result2 = minimumSpanningTreePrim(graph: graph)
