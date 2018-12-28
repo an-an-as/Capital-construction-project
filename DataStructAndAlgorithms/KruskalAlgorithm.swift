98c1f4942ad0d3635ac6601f122e520a698a978c
🍍 <abc88788@hotmail.com>
2018-12-29 03:37:57 +0800

    perf(KruskalAlgorithm.swift): update the content

--- DataStructAndAlgorithms/KruskalAlgorithm.swift
+++ DataStructAndAlgorithms/KruskalAlgorithm.swift
@@ -1,31 +1,3 @@
-///     1    6     4
-///   a -- f -- b -- c
-///             | 3
-///        e -- d
-///           2
-///
-///   c --> d 5
-///   d --> f 7
-
-/// vertices       a  c  b  c  d  e  f
-/// indexDirc     [0, 1, 2, 3, 4, 5, 6]
-/// storage       [0, 1, 2, 3, 4, 5, 6]
-/// size          [1, 2, 3, 4 ,5, 6, 7]
-///
-/// sortedEdge    a-f 1
-///               d-e 2
-///               b-d 3
-///               b-c 4
-///               c-d 5
-///               b-f 6
-///               d-f 7
-///
-/// a0 != f6 false cost: 1  tree.addEdge a-f
-/// union a0 < f6
-/// vertices       a  c  b  c  d  e  f
-/// indexDirc     [0, 1, 2, 3, 4, 5, 6]
-/// storage       [0, 1, 2, 3, 4, 5, 0]
-/// size          [8, 2, 3, 4 ,5, 6, 7]
 struct Edge<T> {
     let vertex1: T
     let vertex2: T
@@ -67,88 +39,123 @@ extension Graph: CustomDebugStringConvertible {
 struct UnionFind<T: Hashable> {
     private var indexDict = [T: Int]()
     private var indexInStorage = [Int]()
-    private var size = [Int]()
+    private var comboSize = [Int]()
     public init() {}
 }
 extension UnionFind {
     mutating func setIndexAndInitialStorage(for element: T) {
         indexDict[element] = indexInStorage.count
         indexInStorage.append(indexInStorage.count)
-        size.append(1)
+        comboSize.append(1)
     }
-    private mutating func findElementInStorage(by index: Int) -> Int {
-        if indexInStorage[index] == index {
-            return index
+    private mutating func getInitialIndexAndTransformOnceAgainVisted(_ index: Int) -> Int {
+        if indexInStorage[index] == index {                 /// 相等为初次查找该值返回其index 连通后 storage[indexB] = indexA  后续 storage[indexA] = indexC
+            return index                                    /// 如果再次查找storage[index]存储新的值 变为
         } else {
-            indexInStorage[index] = findElementInStorage(by: indexInStorage[index])
-            return indexInStorage[index]
-        }
+            indexInStorage[index] = getInitialIndexAndTransformOnceAgainVisted(indexInStorage[index]) /// 转换为后继 a -> b 初次b记录a
+            return indexInStorage[index]                                            /// a -> b -> c -> d   b再次访问后递归到d 这是b==d 就连通
+        }                                                                           //  再一次访问就会变味另一端下标 递归到最后
     }
-    public mutating func findElementInStorage(by element: T) -> Int? {
-        if let index = indexDict[element] {
-            return findElementInStorage(by: index)
-        } else {
-            return nil
-        }
+    public mutating func getIndex(_ element: T) -> Int? {
+        guard let index = indexDict[element] else {return nil }
+        return getInitialIndexAndTransformOnceAgainVisted(index)
     }
-    mutating func isSameInStorage(_ firstElement: T, secondElement: T) -> Bool {
-        if let first = findElementInStorage(by: firstElement), let second = findElementInStorage(by: secondElement) {
-            return first == second
+    mutating func isSameIndex(_ firstElement: T, secondElement: T) -> Bool {
+        if let precursor = getIndex(firstElement), let follower = getIndex(secondElement) {
+            return precursor == follower                /// a -> b  b存储a 再一次访问b 和dict不一致 转换为a a存储c  这时 连同b - c
         } else {
             return false
         }
     }
-    mutating func union(_ firstElement: T, secondElement: T) {
-        guard let first = findElementInStorage(by: firstElement), let second = findElementInStorage(by: secondElement) else { return }
-        guard firstElement != secondElement else { return }
-        indexInStorage.indices.forEach {
-            if indexInStorage[$0] == first { indexInStorage[$0] = second }
-        }
-        size[second] += size[first]
-        /// indexDict "a" -> 0, "b" -> 1
-        /// storage   [0, 1]               union---[1, 1]
-        /// size      [1, 2]               union---[1, 3]
-    }
     mutating func unionSetsContaining(_ firstElement: T, and secondElement: T) {
-        guard let first = findElementInStorage(by: firstElement), let second = findElementInStorage(by: secondElement) else { return }
+        guard let precursor = getIndex(firstElement), let follower = getIndex(secondElement) else { return }
         guard firstElement != secondElement else { return }
-        if size[first] > size[second] {
-            indexInStorage[first] = second
-            size[second] += size[first]
+        if comboSize[precursor] > comboSize[follower] { // 后续记录
+            indexInStorage[precursor] = follower
+            comboSize[follower] += comboSize[precursor]
         } else {
-            indexInStorage[second] = first
-            size[first] += size[second]                                                ///   1    6    4
-        }                                                                              /// a -- f -- b -- c
-    }                                                                                  ///           | 3
-}                                                                                      ///      e -- d
-func minimumSpanningTreeKruskal<T>(graph: Graph<T>) -> (cost: Int, tree: Graph<T>) {   ///        2
-    var cost: Int = 0                                                                  ///
-    var tree = Graph<T>()                                                              ///  c --> d 5
-    let sortedEdgeListByWeight = graph.edgeList.sorted(by: { $0.weight < $1.weight })  ///  d --> f 7
-    var unionFind = UnionFind<T>()                                                     ///
-    for vertex in graph.vertices {                                                     /// vertices       a  c  b  c  d  e  f
-        unionFind.setIndexAndInitialStorage(for: vertex)                               /// indexDirc     [0, 1, 2, 3, 4, 5, 6]
-    }                                                                                  /// storage       [0, 1, 2, 3, 4, 5, 6]
-    for edge in sortedEdgeListByWeight {                                               /// size          [1, 2, 3, 4 ,5, 6, 7]
-        let v1 = edge.vertex1                                                          ///
-        let v2 = edge.vertex2                                                          /// sortedEdge    a-f 1
-        if !unionFind.isSameInStorage(v1, secondElement: v2) {                         ///               d-e 2
-            cost += edge.weight                                                        ///               b-d 3
-            tree.addEdge(edge.vertex1, destionation: edge.vertex2, weight: edge.weight)///               b-c 4
-            unionFind.unionSetsContaining(v1, and: v2)                                 ///               c-d 5
-        }                                                                              ///               b-f 6
-    }                                                                                  ///               d-f 7
-    return (cost: cost, tree: tree)                                                    ///
-}                                                                                      /// a0 != f6 false cost: 1  tree.addEdge a-f
-var graph = Graph<String>()                                                            /// union
-graph.addEdge("a", destionation: "f", weight: 1)
-graph.addEdge("d", destionation: "e", weight: 2)
-graph.addEdge("b", destionation: "d", weight: 3)
-graph.addEdge("b", destionation: "c", weight: 4)
-graph.addEdge("c", destionation: "d", weight: 5)
-graph.addEdge("b", destionation: "f", weight: 6)
-graph.addEdge("d", destionation: "f", weight: 7)
+            indexInStorage[follower] = precursor        // 初次记录
+            comboSize[precursor] += comboSize[follower]
+        }
+    }
+}
+func minimumSpanningTreeKruskal<T>(graph: Graph<T>) -> (cost: Int, tree: Graph<T>) {
+    var cost: Int = 0
+    var tree = Graph<T>()
+    let sortedEdgeListByWeight = graph.edgeList.sorted(by: { $0.weight < $1.weight })
+    var unionFind = UnionFind<T>()
+    for vertex in graph.vertices {
+        unionFind.setIndexAndInitialStorage(for: vertex)
+    }
+    for edge in sortedEdgeListByWeight {
+        let v1 = edge.vertex1
+        let v2 = edge.vertex2
+        if !unionFind.isSameIndex(v1, secondElement: v2) {
+            cost += edge.weight
+            tree.addEdge(edge.vertex1, destionation: edge.vertex2, weight: edge.weight)
+            unionFind.unionSetsContaining(v1, and: v2)
+        }
+    }
+    return (cost: cost, tree: tree)
+}
+var graph = Graph<String>()
+graph.addEdge("a", destionation: "b", weight: 1)
+graph.addEdge("a", destionation: "c", weight: 2)
+graph.addEdge("b", destionation: "c", weight: 3)
+graph.addEdge("b", destionation: "d", weight: 4)
+
 print(graph.vertices)
 let result = minimumSpanningTreeKruskal(graph: graph)
 print(result.tree)
 print(result.cost)
+///       1
+///    a ---> b
+///   2|      |4
+///    c      d
+///
+///    b -- > d: 4
+///
+/// vertices       a  b  c  d
+/// indexDirc     [0, 1, 2, 3]
+/// storage       [0, 1, 2, 3]
+/// size          [1, 1, 1, 1]
+///
+/// sortedEdge    a-b 1
+///               a-c 2
+///               b-c 3
+///               b-d 4
+///
+///           indexInStorage[second] = first
+///           size[first] += size[second]
+/// inStorage[a0]=0 != inStorage[b1]=1 false cost: 1    tree.addEdge a-b
+/// size[a0=0]=0 !> size[b1=1]=1  storage[b0=0] = a2  size[a2=2] += size[b3=3]
+/// vertices       a    b    c    d
+/// indexDict     [0,   1,   2,   3]
+/// storage       [0,  >0<   2,   3]   b 存储 a 的下标0
+/// size          [>2<  1,   1,   1]   a 合并 b 的size1
+///
+/// inStorage[a0]=0 != inStorage[c2]=2 true cost: 1+2   tree.addEdge a-c
+/// size[a0=0]=2 > size[c0=0]=1  storage[a2] = c3  size[c3] += size[a1]
+/// vertices       a    b    c    d
+/// indexDirc     [0,   1,   2,   3]
+/// storage       [2,   0    2,   3]   a 存储 c 的下标0
+/// size          [2,   1,  >3<   1]   c 合并 a 的size2
+///
+/// inStorage inStorage[b0]=2 == inStorage[c2]=2 false 不合并          b indexInDict --> 1  storage[1] = 0 != 1  a -> b 更改了b的下标为a
+///                                                                                        storage[1] = storage[storage[1]] = 2
+/// 查找b
+/// vertices       a    b    c    d
+/// indexDirc     [0,   1,   2,   3]
+/// storage       [2,   2    2,   3]   a 存储 c 的下标0
+/// size          [2,   1,  >3<   1]   c 合并 a 的size2
+///
+/// if size[first] > size[second] { indexInStorage[first] = second    1. a--->b b存储a的下标
+///     size[second] += size[first]                                   2. a--->c a存储c的下标
+/// } else {                                                          3. 查找b已经改变(和Dict中的index不一致) 转换为sstorage[storage[b]] a 存在两个相同下标构成回路
+///     indexInStorage[second] = first
+///     size[first] += size[second]
+/// }
+///  初次(1,1) b的size需要传递到a 后继才能继续  初次的下标通过下一个下标存储storage[b] = a 以便后续判断 -----   再一次用到b的时候则变为storage[a] 比较是否相同构成回路
+///                                                                                             |                                由于按顺序从小添加, a-b a-c避免回路
+///  如果 size a > c 合并到c  c > d 合并到d  有新的节点(size 默认1)就会一直传递  新的下标通过前一个下标存储 storage[a] = c  storage[c] = d
+///
