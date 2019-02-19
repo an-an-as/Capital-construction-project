c94f28b802da03227276527466f036edd0c3ad1b
🍍 <abc88788@hotmail.com>
2019-02-19 20:18:08 +0800

    feat(Graph.swift): version 3

--- DataStructAndAlgorithms/Graph.swift
+++ DataStructAndAlgorithms/Graph.swift
@@ -428,3 +428,359 @@ print(graph.minimanSpanTree())
 print(graph.minimanSapnTree(from: "a"))
 print(graph.shortestPath(from: "a", destination: "f"))
 print(graph.shortestPathLength(from: "a", destination: "f")?.length)
+
+// Mark: - VERSION: 3
+struct UnionFind<Element: Hashable> {
+    var index = [Element: Int]()
+    var next = [Int]()
+    var size = [Int]()
+}
+extension UnionFind {
+    mutating func initial(_ element: Element) {
+        index[element] = next.count
+        next.append(next.count)
+        size.append(1)
+    }
+}
+extension UnionFind {
+    mutating func union(source: Element, destination: Element) {
+        guard source != destination, let sourceIndex = index[source], let destinationIndex = index[destination] else { return }
+        if size[sourceIndex] > size[destinationIndex] {
+            next[sourceIndex] = next[destinationIndex]
+            size[destinationIndex] += size[sourceIndex]
+        } else {
+            next[destinationIndex] = next[sourceIndex]
+            size[sourceIndex] += size[destinationIndex]
+        }
+    }
+    mutating func searchIndex(_ element: Element) -> Int? {
+        guard let sourceIndex = index[element] else { return nil }
+        func recurse(_ index: Int) -> Int {
+            if next[index] == index { return index }
+            next[index] = recurse(next[index])
+            return next[index]
+        }
+        return recurse(sourceIndex)
+    }
+    mutating func hasSameIndex(_ lhs: Element, _ rhs: Element) -> Bool {
+        return searchIndex(lhs) == searchIndex(rhs)
+    }
+}
+struct PriorityQueue<Element> {
+    var storage = [Element]()
+    var priority: (Element, Element) -> Bool
+    init(priority: @escaping (Element, Element) -> Bool) {
+        self.priority = priority
+    }
+}
+extension PriorityQueue {
+    mutating func shiftDown(from currentIndex: Int, endIndex: Int) {
+        var cursor = currentIndex
+        let childrenIndexL = currentIndex * 2 + 1
+        let childrenIndexR = childrenIndexL + 1
+        if childrenIndexL < endIndex && priority(storage[childrenIndexL], storage[cursor]) {
+            cursor = childrenIndexL
+        }
+        if childrenIndexR < endIndex && priority(storage[childrenIndexR], storage[cursor]) {
+            cursor = childrenIndexR
+        }
+        guard cursor != currentIndex else { return }
+        storage.swapAt(cursor, currentIndex)
+        shiftDown(from: cursor, endIndex: endIndex)
+    }
+    mutating func shiftUp(from currentIndex: Int) {
+        var cursor = currentIndex
+        var parentIndex = (currentIndex - 1) / 2
+        let value = storage[currentIndex]
+        while cursor > 0 && priority(storage[cursor], storage[parentIndex]) {
+            storage[cursor] = storage[parentIndex]
+            cursor = parentIndex
+            parentIndex = (cursor - 1) / 2
+        }
+        storage[cursor] = value
+    }
+}
+extension PriorityQueue {
+    mutating func enqueue(_ element: Element) {
+        storage.append(element)
+        shiftUp(from: storage.endIndex - 1)
+    }
+    mutating func dequeue() -> Element? {
+        guard let first = storage.first else { return nil }
+        if storage.count == 1 { return storage.removeLast() }
+        storage[0] = storage.removeLast()
+        shiftDown(from: storage.startIndex, endIndex: storage.endIndex)
+        return first
+    }
+}
+struct Queue<Element> {
+    var storage = [Element?]()
+    var head = 0
+}
+extension Queue {
+    mutating func enqueue(_ element: Element) {
+        storage.append(element)
+    }
+    mutating func dequeue() -> Element? {
+        guard head < storage.count, let first = storage[head] else { return nil }
+        storage[head] = nil
+        head += 1
+        if storage.count > 50 && Double(head)/Double(storage.count) > 0.25 {
+            storage.removeFirst(head)
+            head = 0
+        }
+        return first
+    }
+}
+enum Direction {
+    case direct
+    case undirected
+}
+protocol GraphProtocol: CustomDebugStringConvertible {
+    associatedtype Vertex: Hashable, Comparable
+    typealias Edge = (Vertex, Vertex, Double)
+    var edges: [Edge] { get set }
+    var vertices: Set<Vertex> { get set }
+    var visted: Set<Vertex> { get set }
+    init()
+}
+extension GraphProtocol {
+    mutating func addEdge(_ source: Vertex, destination: Vertex, weight: Double, direction: Direction = .direct) {
+        vertices.insert(source)
+        vertices.insert(destination)
+        edges.append((source, destination, weight))
+        switch direction {
+        case .direct: return
+        case .undirected:
+            edges.append((destination, source, weight))
+        }
+    }
+    mutating func reduceWeight(_ vertices: [Vertex]) -> Double {
+        var temp = [Edge]()
+        for currentIndex in 0..<vertices.count - 1 {
+            let result = edges.filter { $0.0 == vertices[currentIndex] && $0.1 == vertices[currentIndex + 1] }
+            temp.append(contentsOf: result)
+        }
+        return temp.reduce(0) { $0 + $1.2 }
+    }
+    var debugDescription: String {
+        var temp = ""
+        edges.forEach {
+            temp += "\($0.0)-\($0.1): \($0.2)\n"
+        }
+        return temp
+    }
+}
+extension GraphProtocol {
+    mutating func minimumSpanningTree() -> Self {
+        var tree = Self.init()
+        var unionFind = UnionFind<Vertex>()
+        vertices.forEach { unionFind.initial($0) }
+        for edge in edges.sorted(by: { $0.2 < $1.2 }) {
+            guard !unionFind.hasSameIndex(edge.0, edge.1) else { continue }
+            tree.addEdge(edge.0, destination: edge.1, weight: edge.2)
+            unionFind.union(source: edge.0, destination: edge.1)
+        }
+        return tree
+    }
+    mutating func minimumSpaningTree(_ source: Vertex) -> Self? {
+        guard vertices.contains(source) else { return nil }
+        var tree = Self.init()
+        visted.removeAll()
+        visted.insert(source)
+        var queue = PriorityQueue<Edge>(priority: <)
+        edges.filter { $0.0 == source }.forEach { queue.enqueue($0) }
+        while let edge = queue.dequeue() {
+            if visted.contains(edge.1) { continue }
+            visted.insert(edge.1)
+            tree.addEdge(edge.0, destination: edge.1, weight: edge.2)
+            edges.filter { $0.0 == edge.1 }.forEach { queue.enqueue($0) }
+        }
+        return tree
+    }
+}
+extension GraphProtocol {
+    mutating func shortestPath(from source: Vertex, destination: Vertex) -> [Vertex] {
+        guard vertices.contains(source), vertices.contains(destination) else { return [] }
+        var weight = [Vertex: Double]()
+        var precursor = [Vertex: Vertex]()
+        vertices.forEach { weight[$0] = Double.infinity }
+        weight[source] = 0
+        precursor[source] = source
+        for _ in 0..<vertices.count - 1 {
+            var update = false
+            edges.forEach {
+                guard let sourceWeight = weight[$0.0], let destinationWeight = weight[$0.1] else { return }
+                let relaxed = sourceWeight + $0.2
+                if relaxed < destinationWeight {
+                    weight[$0.1] = relaxed
+                    precursor[$0.1] = $0.0
+                    update = true
+                }
+            }
+            guard update else { break }
+        }
+        print(weight)
+        for edge in edges {
+            guard let sourceWeight = weight[edge.0], let destinationWeight = weight[edge.1] else { return [] }
+            if destinationWeight > sourceWeight + edge.2 { return [] }
+        }
+        func recurse(_ destination: Vertex) -> [Vertex] {
+            guard weight[destination] != Double.infinity, let previousVertex = precursor[destination] else { return [] }
+            if previousVertex == destination { return [previousVertex] }
+            return recurse(previousVertex) + [destination]
+        }
+        return recurse(destination)
+    }
+    mutating func shortestPath(_ source: Vertex, destination: Vertex) -> (length: Double, member: [Vertex])? {
+        guard vertices.contains(source), vertices.contains(destination) else { return nil }
+        var path = [Vertex: (length: Double, member: [Vertex])]()
+        vertices.forEach { path[$0] = (Double.infinity, []) }
+        path[source] = (0, [source])
+        visted = vertices
+        var vertex: Vertex? = source
+        while let currentVertex = vertex {
+            visted.remove(currentVertex)
+            edges.forEach {
+                guard let sourceVertex = path[$0.0], let destinationVertex = path[$0.1] else { return }
+                let relaxed = sourceVertex.length + $0.2
+                if relaxed < destinationVertex.length {
+                    path[$0.1]?.length = relaxed
+                    path[$0.1]?.member = sourceVertex.member
+                    path[$0.1]?.member.append($0.1)
+                    
+                }
+                vertex = visted.min { path[$0]!.length < path[$1]!.length }
+            }
+        }
+        return path[destination]
+    }
+}
+enum SearchPriority {
+    case breadth
+    case deepth
+}
+extension GraphProtocol {
+    mutating func search(_ source: Vertex, priority: SearchPriority = .breadth) -> [Vertex] {
+        guard vertices.contains(source) else { return [] }
+        var temp = [source]
+        visted.insert(source)
+        switch priority {
+        case .breadth:
+            var queue = Queue<Vertex>()
+            queue.enqueue(source)
+            while let vertex = queue.dequeue() {
+                for edge in (edges.filter { $0.0 == vertex }) where !visted.contains(edge.1) {
+                    temp.append(edge.1)
+                    queue.enqueue(edge.1)
+                    visted.insert(edge.1)
+                }
+            }
+        case .deepth:
+            for edge in (edges.filter { $0.0 == source }) where !visted.contains(edge.1) {
+                temp += search(edge.1, priority: .deepth)
+            }
+        }
+        return temp
+    }
+    mutating func clearSearchResult() {
+        visted.removeAll()
+    }
+}
+struct BasicGraph: GraphProtocol {
+    typealias Vertex = String
+    var edges: [(String, String, Double)]
+    var vertices: Set<String>
+    var visted: Set<String>
+    init() {
+        edges = [Edge]()
+        vertices = Set<Vertex>()
+        visted = Set<Vertex>()
+    }
+}
+var grpah = BasicGraph()
+grpah.addEdge("a", destination: "b", weight: 100)
+grpah.addEdge("b", destination: "c", weight: 3)
+grpah.addEdge("a", destination: "d", weight: 2_000)
+//print(grpah.minimumSpanningTree())
+//print(grpah.minimumSpaningTree("a"))
+//print(grpah.reduceWeight(grpah.shortestPath(from: "a", destination: "c")))
+//print(grpah.shortestPath("a", destination: "c")?.member)
+grpah.clearSearchResult()
+print(grpah.search("a"))
+grpah.clearSearchResult()
+print(grpah.search("a", priority: .deepth))
+
+struct AdjacencyMatrix: GraphProtocol {
+    typealias Vertex = String
+    var edges: [(String, String, Double)]
+    var vertices: Set<String>
+    var visted: Set<String>
+    var index = [Vertex: Int]()
+    var matrix = [[Double?]]()
+    init() {
+        edges = [Edge]()
+        vertices = Set<Vertex>()
+        visted = Set<Vertex>()
+    }
+}
+extension AdjacencyMatrix {
+    mutating func addVertex(_ vertex: Vertex) {
+        matrix.indices.forEach { matrix[$0].append(nil) }
+        index[vertex] = matrix.count
+        let newRow = [Double?](repeating: nil, count: matrix.count.advanced(by: 1))
+        matrix.append(newRow)
+        
+    }
+    /// Matrix
+    mutating func addEdge(_ source: String, destination: String, weight: Double, direction: Direction = .direct) {
+        if vertices.insert(source).inserted { addVertex(source) }
+        if vertices.insert(destination).inserted { addVertex(destination) }
+        edges.append((source, destination, weight))
+        guard let sourceIndex = index[source], let destinationIndex = index[destination] else { return }
+        matrix[sourceIndex][destinationIndex] = weight
+        switch direction {
+        case .direct: return
+        case .undirected:
+            matrix[destinationIndex][sourceIndex] = weight
+            edges.append((destination, source, weight))
+        }
+    }
+    mutating func getWeight(_ source: Vertex, destination: Vertex) -> Double? {
+        guard let sourceIndex = index[source], let destinationIndex = index[destination] else { return nil }
+        return matrix[sourceIndex][destinationIndex]
+    }
+}
+struct AdjacencyLinkedList: GraphProtocol {
+    typealias Vertex = String
+    var edges: [(String, String, Double)]
+    var vertices: Set<String>
+    var visted: Set<String>
+    var list = [Vertex: [(Vertex, Double)]]()
+    init() {
+        edges = [Edge]()
+        vertices = Set<Vertex>()
+        visted = Set<Vertex>()
+    }
+}
+extension AdjacencyLinkedList {
+    /// List
+    mutating func addEdge(_ source: String, destination: String, weight: Double, direction: Direction = .direct) {
+        vertices.insert(source)
+        vertices.insert(destination)
+        edges.append((source, destination, weight))
+        list[source] = list[source] ?? []
+        list[source]?.append((destination, weight))
+        switch direction {
+        case .direct: return
+        case .undirected:
+            edges.append((destination, source, weight ))
+            list[destination] = list[destination] ?? []
+            list[destination]?.append((source, weight))
+        }
+    }
+    subscript(vertex: Vertex) -> [(Vertex, Double)]? {
+        guard vertices.contains(vertex) else { return nil }
+        return list[vertex]
+    }
+}
