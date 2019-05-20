7a7a6adf8596b7fb58171f538399989f0ec12a4b
🍍 <abc88788@hotmail.com>
2019-05-20 17:08:55 +0800

    feat(Unicode): ✨initial commit

---
+++ Components/Unicode/UnicodeName.swift
@@ -0,0 +1,12 @@
+///Unicode标量转换为对应的官方Unicode名字
+extension Unicode.Scalar {
+    var unicodeName: String {
+        let name = String(self).applyingTransform(.toUnicodeName, reverse: false)!
+        let prefixPattern = "\\N{"
+        let suffixPattern = "}"
+        let prefixLength = name.hasPrefix(prefixPattern) ? prefixPattern.count : 0
+        let suffixLength = name.hasSuffix(suffixPattern) ? suffixPattern.count : 0
+        return String(name.dropFirst(prefixLength).dropLast(suffixLength))
+    }
+}
+"😊".unicodeScalars.map { $0.unicodeName }
